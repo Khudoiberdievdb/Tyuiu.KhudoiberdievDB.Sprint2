1b93317b4a56131121b458a61d1c26d9e9275b8f
Tyuiu.HudojberdievDB@outlook.com <Tyuiu.HudojberdievDB@outlook.com>
2024-11-14 14:45:35 +0300

    SprintRewiew2V12

---
+++ Tyuiu.KhudoiberdievDB.SprintRewiew2.V12/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Tyuiu.KhudoiberdievDB.SprintRewiew2.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Худойбердиев Д. Б. | ИСПБ 24-1";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                    *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                             *");
+            Console.WriteLine("* Задание #SprintRewiew                                                        *");
+            Console.WriteLine("* Вариант #12                                                                  *");
+            Console.WriteLine("* Выполнил: Худойбердиев Далер Бахритдинович | ИСПБ 24-1                       *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Написать программу, которая запрашивает исходные данные и вычисляет,         *");
+            Console.WriteLine("* нахожится ли точка с координатами X,Y в закрашенное области                  *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("Введите значение переменной X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+
+            bool res;
+            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && ((x > 0) && (x <= 1)) && ((y > 0) && (y <= 0.5)))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            
+
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+            if (res)
+            {
+                Console.WriteLine("Точка находится в закрашенной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка находится вне закрашенной области");
+            }
+            Console.ReadKey();
+        }
+    }
+}
